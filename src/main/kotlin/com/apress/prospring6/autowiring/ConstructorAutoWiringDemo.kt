55c506462b9cf6160c357126992e40abe5f26737
Prasant <prasant10050@gmail.com>
2024-06-12 12:55:53 +0530

    AutoWiring your bean by type

--- src/main/kotlin/com/apress/prospring6/autowiring/ConstructorAutoWiringDemo.kt
+++ src/main/kotlin/com/apress/prospring6/autowiring/ConstructorAutoWiringDemo.kt
@@ -3,64 +3,115 @@ package com.apress.prospring6.autowiring
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.annotation.AnnotationConfigApplicationContext
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Lazy
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.context.annotation.*
 import org.springframework.stereotype.Component
-import java.util.UUID
+import java.util.*
 
 object ConstructorAutoWiringDemo {
-    private val logger:Logger=LoggerFactory.getLogger(ConstructorAutoWiringDemo::class.java)
+    private val logger: Logger =
+        LoggerFactory.getLogger(ConstructorAutoWiringDemo::class.java)
+
     @JvmStatic
     fun main(args: Array<String>) {
-        val ctx=AnnotationConfigApplicationContext(AutowiringCfg::class.java)
-        val target=ctx.getBean(Target::class.java)
-        logger.info("Created target? {}",target!=null)
-        logger.info("Injected bar? {}",target.bar!=null)
-         logger.info("Injected fooOne? {}", if (target.fooOne != null) target.fooOne!!.id else "")
-        logger.info("Injected fooTwo? {}", if (target.fooTwo != null) target.fooTwo!!.id else "")
+        val ctx = AnnotationConfigApplicationContext(AutowiringCfg::class.java)
+        val target = ctx.getBean(Target::class.java)
+        logger.info("Created target? {}", target != null)
+        logger.info("Injected bar? {}", target.bar != null)
+        logger.info(
+            "Injected fooOne? {}",
+            if (target.fooOne != null) target.fooOne!!.id else ""
+        )
+        logger.info(
+            "Injected fooTwo? {}",
+            if (target.fooTwo != null) target.fooTwo!!.id else ""
+        )
+        logger.info("------------------------------------------------------")
+        // ByType auto confirguration
+        val anotherTarget = ctx.getBean(AnotherTarget::class.java)
+        logger.info("Created target? {}", anotherTarget != null)
+        logger.info("Injected bar? {}", anotherTarget.bar != null)
+        logger.info(
+            "Injected fooOne? {}",
+            if (anotherTarget.fooOne != null) anotherTarget.fooOne!!.id else ""
+        )
+        logger.info(
+            "Injected fooTwo? {}",
+            if (anotherTarget.fooTwo != null) anotherTarget.fooTwo!!.id else ""
+        )
     }
 }
 
 @Configuration
 @ComponentScan
-internal open class AutowiringCfg
+internal open class AutowiringCfg{
+    @Bean
+    open fun anotherFoo(): Foo {
+        return Foo()
+    }
+}
 
 @Component
 @Lazy
-internal class Target{
-    var fooOne: Foo?=null
-    var fooTwo: Foo?=null
-    var bar: Bar?=null
+internal class Target {
+    var fooOne: Foo? = null
+    var fooTwo: Foo? = null
+    var bar: Bar? = null
 
-    constructor(){
+    constructor() {
         logger.info("---> Target() called")
     }
 
     //@Autowired
-    constructor(foo: Foo){
-        fooOne=foo
+    constructor(foo: Foo) {
+        fooOne = foo
         logger.info("---> Target(Foo) called")
     }
 
     //@Autowired
-    constructor(foo: Foo,bar:Bar){
-        fooOne=foo
-        this.bar=bar
+    constructor(foo: Foo, bar: Bar) {
+        fooOne = foo
+        this.bar = bar
         logger.info("---> Target(Foo,Bar) called")
     }
 
-    companion object{
-        private val logger:Logger=LoggerFactory.getLogger(Target::class.java)
+    companion object {
+        private val logger: Logger = LoggerFactory.getLogger(Target::class.java)
     }
 
 }
 
 @Component
-internal class Foo{
-    var id=UUID.randomUUID().toString().replace("-","").substring(0,8)
+internal class Foo {
+    var id = UUID.randomUUID().toString().replace("-", "").substring(0, 8)
 }
 
 @Component
-internal class Bar{}
+internal class Bar {}
+
+@Component
+@Lazy
+internal class AnotherTarget {
+    var fooOne: Foo? = null
+        @Autowired set(@Qualifier("foo") value) {
+            logger.info("---> AnotherTarget#setFooOne(Foo) called")
+            field = value
+        }
+
+    var fooTwo: Foo? = null
+        @Autowired set(@Qualifier("anotherFoo") value) {
+            logger.info("---> AnotherTarget#setFooTwo(Foo) called")
+            field = value
+        }
+
+    var bar: Bar? = null
+        @Autowired set(value) {
+            logger.info("---> AnotherTarget#setBar(Bar) called")
+            field = value
+        }
+
+    companion object {
+        private val logger: Logger =
+            LoggerFactory.getLogger(AnotherTarget::class.java)
+    }
+}
